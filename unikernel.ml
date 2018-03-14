b26834e4a8e5d55bcc951c372a2e3a257b45873c
Reynir Björnsson <reynir@reynir.dk>
2018-03-14 15:40:08 +0100

    Read precise input

--- unikernel.ml
+++ unikernel.ml
@@ -10,33 +10,41 @@ let with_faraday (f : Faraday.t -> unit) : string =
   f buf;
   Faraday.serialize_to_string buf
 
-let rec handler ~user command flow : int Lwt.t =
+let bigstring_of_unconsumed { Angstrom.Buffered.buf; len; off } =
+  Bigarray.Array1.sub buf off len
+
+let handler ~user command flow : int Lwt.t =
   Log.info (fun f -> f "Connection %S:%S\n" user command);
   let prefix' = String.sub command 0 (String.length prefix) in
   if prefix <> prefix'
   then begin Log.info (fun f -> f "Wrong prefix");
     Lwt.return 0 (* Ignore, I guess *) end
-  else Qubes.RExec.Flow.read flow >>= function
-    | `Eof ->
-      Log.info (fun f -> f "EOF");
-      Lwt.return 0
-    | `Ok raw_request ->
-      Log.info (fun f -> f "Got a request of len %d" (String.length (Cstruct.to_string raw_request)));
-      match Angstrom.parse_string
-              Ssh_agent.Parse.ssh_agentc_message
-              (Cstruct.to_string raw_request) with
-      | Error e ->
-        Log.err (fun f -> f "Error parsing request %s" e);
-        Lwt.return 1
-      | Ok (Ssh_agent.Any_request req) ->
-        Log.info (fun f -> f "Succesfully parsed");
+  else
+    let rec loop (state : Ssh_agent.any_ssh_agent_request Angstrom.Buffered.state) =
+      match state with
+      | Angstrom.Buffered.Done (u, Ssh_agent.Any_request req) ->
         let resp = Agent.handler req in
-        Log.info (fun f -> f "Writing response...");
-        let resp =
-          with_faraday (fun t ->
-              Ssh_agent.Serialize.write_ssh_agent_response t resp) in
+        let resp = with_faraday (fun t ->
+            Ssh_agent.Serialize.write_ssh_agent_response t resp) in
         Qubes.RExec.Flow.write flow (Cstruct.of_string resp) >>= fun () ->
-        handler ~user command flow
+        let state = Angstrom.Buffered.parse Ssh_agent.Parse.ssh_agentc_message in
+        let state = Angstrom.Buffered.feed state
+            (`Bigstring (bigstring_of_unconsumed u)) in
+        loop state
+      | Angstrom.Buffered.Partial _ ->
+        Qubes.RExec.Flow.read flow >>= begin function
+          | `Eof ->
+            let state = Angstrom.Buffered.feed state `Eof in
+            loop state
+          | `Ok input ->
+            let state = Angstrom.Buffered.feed state (`Bigstring (Cstruct.to_bigarray input)) in
+            loop state
+        end
+      | Angstrom.Buffered.Fail (_, _, e) ->
+        Log.err (fun f -> f "Error parsing request: %s" e);
+        Lwt.return 1
+    in
+    loop (Angstrom.Buffered.parse Ssh_agent.Parse.ssh_agentc_message)
 
 module Main (DB : Qubes.S.DB) = struct
 
