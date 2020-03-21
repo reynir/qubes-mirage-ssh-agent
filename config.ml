2b8237298a020aaba9254ec84886260dd400bb14
Reynir Björnsson <reynir@reynir.dk>
2020-03-21 18:02:28 +0100

    Add incantations to seed the RNG

--- config.ml
+++ config.ml
@@ -6,10 +6,11 @@ let main =
     package "mirage-qubes";
     package ~min:"0.3.0" "ssh-agent";
     package "angstrom";
+    package "mirage-crypto-rng";
     package "mirage-crypto-pk";
   ] in
   foreign ~packages
-    "Unikernel.Main" (qubesdb @-> job)
+    "Unikernel.Main" (random @-> qubesdb @-> job)
 
 let () =
-  register "qubes-ssh-agent" ~argv:no_argv [ main $ default_qubesdb ]
+  register "qubes-ssh-agent" ~argv:no_argv [ main $ default_random $ default_qubesdb ]
